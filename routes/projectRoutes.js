da0a7d7236d270b53c9fa42c58ccc71937b6a480
masonkarsevar <masonkarsevar@gmail.com>
2019-09-06 08:36:09 -0700

    basic individual id get request finished. WIP: validateId middleware

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -15,4 +15,14 @@ router.get('/', (req, res) => {
         })
 });
 
+router.get('/:id', (req, res) => {
+    db.get(req.params.id) 
+        .then(results => {
+            res.status(200).json(results);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+})
+
 module.exports = router;
