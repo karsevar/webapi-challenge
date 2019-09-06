88ffd4c5110721b5057541eca83bc8e3866d6ee1
masonkarsevar <masonkarsevar@gmail.com>
2019-09-06 09:34:24 -0700

    added delete functionality for the project

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,33 +1,86 @@
 const express = require('express');
-const db = require('../data/helpers/actionModel.js');
+const actionDb = require('../data/helpers/actionModel.js');
+const projectDb = require('../data/helpers/projectModel.js')
 
 const router = express.Router();
 
 router.use(express.json());
 
 router.get('/', (req, res) => {
-    db.get()
+    projectDb.get()
         .then(results => {
-            res.status(200).json(results);
+            res.status(200).json(results) 
+        })
+        .catch(error => {
+            res.status(500).json(error) 
+        })
+});
+
+router.get('/:id', validateProjectId, (req, res) => {
+    projectDb.get(req.params.id) 
+        .then(results => {
+            res.status(200).json(results) 
+        })
+        .catch(error => {
+            res.status(500).json(error) 
+        })
+});
+
+router.post('/', validateProjectPost, (req, res) => {
+    projectDb.insert(req.body) 
+        .then(results => {
+            res.status(200).json(results)
         })
         .catch(error => {
             res.status(500).json(error)
         })
 });
 
-router.get('/:id', validateId, (req, res) => {
-    db.get(req.params.id) 
+router.delete('/:id', validateProjectId, (req, res) => {
+    projectDb.remove(req.params.id) 
         .then(results => {
-            res.status(200).json(results);
+            res.status(201).json(results)
         })
         .catch(error => {
-            res.status(500).json(error);
+            res.status(500).json(error) 
         })
 });
 
+
+
+// router.get('/:id', (req, res) => {
+//     actionDb.get()
+//         .then(results => {
+//             res.status(200).json(results);
+//         })
+//         .catch(error => {
+//             res.status(500).json(error)
+//         })
+// });
+
+// router.get('/:id', validateId, (req, res) => {
+//     actionDb.get(req.params.id) 
+//         .then(results => {
+//             res.status(200).json(results);
+//         })
+//         .catch(error => {
+//             res.status(500).json(error);
+//         })
+// });
+
+// router.post('/', validatePost, (req, res) => {
+//     actionDb.insert(req.body) 
+//         .then(results => {
+//             res.status(200).json(results)
+//         })
+//         .catch(error => {
+//             res.status(500).json(error)
+//         })
+// })
+
 // middleware:
 function validateId(req, res, next) {
-    db.get(req.params.id) 
+    actionDb.get(req.params.id) 
         .then(results => {
             if(typeof results === 'object') {
                 console.log(`${req.params.id} is an object`)
@@ -36,6 +89,38 @@ function validateId(req, res, next) {
                 res.status(400).json({ message: "invalid project id" })
             }
         })
+}
+
+function validateProjectId(req, res, next) {
+    projectDb.get(req.params.id) 
+        .then(results => {
+            if(results === null) {
+                res.status(400).json({message: "invalid project id"})
+            } else {
+                next();
+            }
+        })
+}
+
+function validateProjectPost(req, res, next) {
+    if(req.body.name && req.body.description) {
+        next();
+    } else {
+        res.status(400).json({message: "missing required description or name field"})
+    }
+}
+
+function validatePost(req, res, next) {
+    if(req.body.notes && req.body.description) {
+        console.log(`Length of the description ${req.body.description.length}`)
+        if(req.body.description.length <= 128) {
+            next();
+        } else {
+            res.status(400).json({message: "description field needs to be less than 128 characters"})
+        }
+    } else {
+        res.status(400).json({message: "missing required description or notes field"})
+    }
 }
 
 module.exports = router;
